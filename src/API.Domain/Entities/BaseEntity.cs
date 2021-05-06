1dfa96a808057e5b47d4f785c0e36a5b07af2b38
Marina Julia <marinajulia707@gmail.com>
2021-05-06 14:26:12 -0300

    User Entity

---
+++ src/API.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public Guid id { get; set; }
+        //? pode receber nulo
+        private DateTime? _createAt;
+        public DateTime? MyProperty
+        {
+            get { return _createAt; }
+            set { _createAt = (value == null ? DateTime.UtcNow : value); } // se for nulo, createAt recebe UtcNow (que é o que tá configurado no servidor)Obtém um objeto DateTime definido como a data e hora atual neste computador, expressas como o UTC (Tempo Universal Coordenado).
+        }
+
+        public DateTime UpdateAt { get; set; }
+    }
+}
