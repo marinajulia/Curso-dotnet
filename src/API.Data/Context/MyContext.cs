c16191b164102a8d4333161b5ebae4e8512bfdde
Marina Julia <marinajulia707@gmail.com>
2021-05-07 17:44:59 -0300

    Alterações

--- src/API.Data/Context/MyContext.cs
+++ src/API.Data/Context/MyContext.cs
@@ -1,7 +1,17 @@
+using API.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace API.Data.Context
-{
-    public class MyContext
+{//vai receber as tabelas relacionadas a entidade e faz um conjunto com o EntityFramework
+    public class MyContext : DbContext
     {
-        
+        public DbSet<UserEntity> Users { get; set; }//recebe a referrência da tabela entidade
+
+        public MyContext(DbContextOptions<MyContext> options) : base(options) { }//construtor
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
