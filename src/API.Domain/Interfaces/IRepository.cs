2ddd7ee2907f6af0f836ccb11e4d828a9338b3b2
Marina Julia <marinajulia707@gmail.com>
2021-05-11 17:40:07 -0300

    criando interface de repository

---
+++ src/API.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.Domain.Entities;
+
+namespace API.Domain.Interfaces
+{//como vai ser uma interface de um repositório, ela deve ser um repositório padrão
+ //o T no IRepository representa um repositório genérico
+    public interface IRepository<T> where T : BaseEntity //onde T seja um tipo de BaseEntity //use api domain Entities
+    {
+        //Task será uma tarefa
+        //InsertAsync faz com que pode executar vários de uma vez
+        //colocar Threading na task
+        //nas tasks são devovidos os "T"
+        Task<T> InsertAsync(T item); //aqui ele receber uma entidade(t) parametro (item)
+        Task<T> UpdateAsync(T item);
+        Task<bool> DeleteAsync(Guid id);//using system 
+        Task<T> SelectAsync(Guid id);
+        //vai trazer uma lista com o IEnumerable
+        Task<IEnumerable<T>> SelectAsync(); //System.Collections.Generic;
+    }
+}
